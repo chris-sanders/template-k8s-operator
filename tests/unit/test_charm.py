9785a9907c16d984366c41db617d1651c22e83ab
Chris Sanders <sanders.chris@gmail.com>
2020-05-21 16:43:13 -0500

    Start harness switch

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -1,18 +1,20 @@
 import unittest
 
 import setuppath  # noqa:F401
-from operator_fixtures import OperatorTestCase
+from ops.testing import Harness
 
 
-class TestCharm(OperatorTestCase):
-    def test_create_charm(self):
-        """Verify fixtures and create a charm."""
-        self.assertEqual(self.charm.state.installed, False)
+class TestCharm(unittest.TestCase):
+    def test_harness(self):
+        """Verify harness."""
+        harness = Harness(${class})
+        harness.begin()
+        self.assertFalse(harness.charm.state.installed)
 
-    def test_install(self):
-        """Test emitting an install hook."""
-        self.emit("install")
-        self.assertEqual(self.charm.state.installed, True)
+    # def test_install(self):
+    #     """Test emitting an install hook."""
+    #     self.emit("install")
+    #     self.assertEqual(self.charm.state.installed, True)
 
 
 if __name__ == "__main__":
